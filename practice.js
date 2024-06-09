cfc399e12430db03b7bfc0eff07fd03f66d1817f
Julio Lopez <58756482+donlopez@users.noreply.github.com>
2024-06-09 10:49:19 -0400

    new file:   practice.js 	modified:   tempCodeRunnerFile.js

---
+++ practice.js
@@ -0,0 +1,25 @@
+for (let i = 0; i < 3; i++) {
+    process.stdout.write(i + ' ');
+}
+
+
+for (let i = 0; i < 2; i++) {
+    console.log(i);
+}
+
+
+for (let i = 0; i < 3; i++) {
+    console.log(i * 2 + ' ');
+}
+
+
+for (let i = 0; i >= -3; i--) {
+    console.log(i);
+}
+
+let output = '';
+for (let i = 0; i > -4; i--) {
+    output += i + ' ';
+}
+console.log(output.trim());
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,5 @@
-initial_savings = 10000
-interest_rate = 0.05
-
-years = int(input('Enter years: '))
-print()
-
-savings = initial_savings
-for i in range(years):
-    print(f' Savings in year {i}: ${savings:.2f}')
-    savings = savings + (savings*interest_rate)
-
-print('\n')
+let output = '';
+for (let i = 0; i > -4; i--) {
+    output += i + ' ';
+}
+console.log(output.trim());
