2ef6b3e21bfd1f58204659c693a6ce95a72c9771
Julio Lopez <58756482+donlopez@users.noreply.github.com>
2024-09-22 13:29:31 -0400

    Update tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,16 @@
-let output = '';
-for (let i = 0; i > -4; i--) {
-    output += i + ' ';
+class YourBlock extends Block {
+    constructor(x, y, size, color1, color2) {
+        super(x, y, size);
+        this.color1 = color1;
+        this.color2 = color2;
+    }
+
+    draw() {
+        // Your custom drawing code here
+        fill(this.color1);
+        rect(this.x, this.y, this.size / 2, this.size / 2);
+        fill(this.color2);
+        rect(this.x + this.size / 2, this.y, this.size / 2, this.size / 2);
+        // Add more shapes as needed
+    }
 }
-console.log(output.trim());
